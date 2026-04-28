1b2847d052db1952528d761862fda4f2da6fc155
nicolasyamaoka <nicolas.yamaoka@unifesp.br>
2026-04-28 09:33:52 -0300

    Update ex91.c

--- ex91.c
+++ ex91.c
@@ -1,26 +1,59 @@
 #include <stdio.h>
 
 int main() {
-    int VET[10], i, j;
-    int p[20], contador = 0;
+    int VET[10], i, j, k, l;
+    int p[20], n[20], contn = 0, contp = 0;
+    char statusPi = 'V' , statusPj = 'V';
+
+    for(k=0;k<20;k++){
+        p[k]= -99999;
+        n[k]= -99999;
+    }
 
     for(i = 0; i < 10; i++){
-        printf("\nEscreva o numero %d do vetor VET -> ", i + 1);
+        printf("\nEscreva o numero %d do vetor VET -> ", i);
         scanf("%d", &VET[i]);
     }
 
-    for(i = 0; i < 9; i++){
+    for(i = 0; i<10;i++){ 
         for(j = i + 1; j < 10; j++){
+
             if(VET[i] == VET[j]){
-                p[contador++] = i;
-                p[contador++] = j;
-            }
+
+                for(k = 0; k <20 ; k++){
+                    if(i == p[k]){
+                    statusPi = 'F';
+                    }
+                }
+
+                for(k = 0; k <20 ; k++){
+                    if(j == p[k]){
+                    statusPj = 'F';
+                    }
+                }
+
+                if(statusPi == 'V'){
+                    p[contp++] = i; 
+                    n[contn++] = VET[i]; 
+                }
+
+                if(statusPj == 'V'){
+                    p[contp++] = j;
+                    n[contn++] = VET[i]; 
+                }             
+
+            }   
         }
+        statusPi = 'V';
+        statusPj = 'V';
     }
 
+
+
+
     printf("\nAs posicoes dos numeros que se repetem sao:");
-    for(i = 0; i < contador; i += 2){
-        printf("\nPosicao %d e Posicao %d", p[i], p[i+1]);
+    for(i = 0; i < contp; i++){
+        printf("\nO número %d se repete na posição %d", n[i], p[i]);
     }
 
     return 0;
